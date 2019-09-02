c1bee14732feb48be5d58c950a7cec92ccd473a0
Lenard Carroll <52955678+crrlen001@users.noreply.github.com>
2019-09-02 19:53:08 +0200

    Delete fix-header-of-gjf.sh

--- project/conformers/fix-header-of-gjf.sh
+++
@@ -1,2 +0,0 @@
-#If you have multiple conformers, z is the number of conformers below
-for i in {1..z}; do sed -i "s/%chk=conf-x.chk/%chk=conf-${i}.chk/" conf-${i}.gjf; sed -i "s/ 4-methylimidazole.log/4-methylimidazole (${i})/" conf-${i}.gjf; sed -i 's/-1  1/-1 1/' conf-${i}.gjf; done
